421af91b3a105736432e512ce55e4c0aa8d0492d
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2026-03-17 13:31:00 +0900

    chore: 프로젝트 설정 update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import rehypeMermaid from 'rehype-mermaid';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chuz.dev',
   base: '/',
   integrations: [sitemap()],
   markdown: {
-    rehypePlugins: [rehypeMermaid],
+    rehypePlugins: [],
     shikiConfig: {
       theme: 'css-variables',
       langs: [],
